8ee1fb5898dde471934c5e82f927a121bd73c98f
Rita Bancevicius <rita@ritute.com>
2021-05-28 12:16:00 -0400

    Fix scroll and add analytics (google + hotjar)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import {
   makeStyles,
-  Container,
-  Toolbar,
-  Grid,
   Typography,
-  AppBar,
-  Button,
-  Drawer,
-  Hidden,
-  ListItem,
-  ListItemText,
-  List,
-  IconButton,
 } from '@material-ui/core'
-import { Menu as MenuIcon } from '@material-ui/icons'
-import { HashLink as Link } from 'react-router-hash-link'
 import { Fade } from 'react-awesome-reveal'
 
 import Avatar from './Avatar'
-import SocialIcons from './SocialIcons'
+import Nav from './Nav'
 import backgroundVideo from 'videos/background.mp4'
 
 const useStyles = makeStyles((theme) => ({
@@ -44,13 +31,6 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     height: '100%',
     opacity: 0.5,
-  },
-  appBar: {
-    background: 'none',
-    '& button': {
-      color: theme.palette.common.white,
-      textTransform: 'none',
-    },
   },
   avatarContainer: {
     borderRadius: '50%',
@@ -88,107 +68,7 @@ const useStyles = makeStyles((theme) => ({
       top: '20%',
     },
   },
-  navLinks: {
-    '& a': {
-      color: theme.palette.common.white,
-      textTransform: 'none',
-      transition: 'all .5s ease',
-      '&:hover': {
-        opacity: 0.8,
-        transform: 'scale(1.1)',
-      }
-    }
-  },
-  drawerPaper: {
-    background: theme.palette.common.black,
-    color: theme.palette.common.white,
-    '& a': {
-      padding: theme.spacing(1, 4)
-    }
-  },
-  toolbar: {
-    [theme.breakpoints.down('xs')]: {
-      padding: 0,
-    }
-  },
 }))
-
-const navLinks = [
-  {
-    title: 'Work',
-    link: '#work'
-  },
-  {
-    title: 'About',
-    link: '#about'
-  },
-  {
-    title: 'Contact',
-    link: '#contact'
-  },
-]
-
-const NavDrawer = ({ onClick }) => {
-  return (
-    <nav>
-      <List>
-        {navLinks.map(({ link, title }, index) => (
-          <ListItem button key={index} component={Link} smooth to={link} onClick={onClick}>
-            <ListItemText primary={title} />
-          </ListItem>
-        ))}
-      </List>
-    </nav>
-  )
-}
-
-const Nav = () => {
-  const classes = useStyles()
-  const [mobileOpen, setMobileOpen] = useState(false)
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen)
-  }
-
-  return (
-    <AppBar position='absolute' elevation={0} className={classes.appBar}>
-      <Toolbar className={classes.toolbar}>
-        <Container maxWidth='lg'>
-          <Grid container alignItems='center' justify='space-between'>
-            <Grid item className={classes.navLinks}>
-              <Hidden xsDown>
-                <nav>
-                  {navLinks.map(({ link, title }, index) => (
-                    <Button key={index} component={Link} smooth to={link}>{title}</Button>
-                  ))}
-                </nav>
-              </Hidden>
-              <Hidden smUp implementation='css'>
-                <IconButton onClick={handleDrawerToggle}><MenuIcon /></IconButton>
-                <Drawer
-                  variant='temporary'
-                  anchor='left'
-                  open={mobileOpen}
-                  onClose={handleDrawerToggle}
-                  classes={{
-                    paper: classes.drawerPaper,
-                  }}
-                  ModalProps={{
-                    keepMounted: true, // better open performance on mobile
-                  }}>
-                  <NavDrawer onClick={handleDrawerToggle} />
-                </Drawer>
-              </Hidden>
-            </Grid>
-            <Grid item>
-              <SocialIcons />
-            </Grid>
-          </Grid>
-        </Container>
-      </Toolbar>
-    </AppBar>
-  )
-}
 
 const BackgroundVideo = () => {
   const classes = useStyles()
